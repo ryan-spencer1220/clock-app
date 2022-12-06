c5720db8f639baa32da78f5d6c3cf766acf7f042
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-12-05 20:34:48 -0800

    add world time api call

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [quote, setQuote] = useState({});
+  const [date, setDate] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch("https://api.quotable.io/random")
       .then((res) => res.json())
-      .then((json) => console.log(json))
-      .then((quote) => setQuote(quote))
-      .then(console.log(quote));
-  }, [quote]);
+      .then((quote) => setQuote(quote));
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://worldtimeapi.org/api/ip")
+      .then((res) => res.json())
+      .then((date) => setDate(date));
+    setLoading(false);
+  }, []);
 
   return (
     <div className="app-mobile">
-      {quote ? <p>{quote.content}</p> : <p>dogs</p>}
+      {loading ? <p>Loading!</p> : <h5>{quote.content}</h5>}
       <p>Good MORNING, IT'S CURRENTLY</p>
       <h1>11:37</h1>
       <p></p>
