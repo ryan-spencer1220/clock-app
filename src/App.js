e78a4fb70c42386fdc043f93b5dbd8145d41a824
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-12-06 21:10:35 -0800

    add icons

--- src/App.js
+++ src/App.js
@@ -1,49 +1,64 @@
 import { useEffect, useState } from "react";
+import { HiSun } from "react-icons/hi";
+import { HiMoon } from "react-icons/hi";
 
 function App() {
   const [quote, setQuote] = useState({});
   const [date, setDate] = useState({});
   const [loading, setLoading] = useState(false);
-  const [loading2, setLoading2] = useState(false);
+  // const [loading2, setLoading2] = useState(false);
 
   const fetchDate = async () => {
-    setLoading2(true);
     fetch("http://worldtimeapi.org/api/ip")
       .then((res) => res.json())
-      .then((date) => setDate(date))
-      .then(console.log(date));
-    setLoading2(false);
+      .then((date) => setDate(date));
   };
 
   const fetchQuote = async () => {
     setLoading(true);
     fetch("https://api.quotable.io/random")
       .then((res) => res.json())
-      .then((quote) => setQuote(quote))
-      .then(console.log(quote));
+      .then((quote) => setQuote(quote));
     setLoading(false);
   };
 
   useEffect(() => {
     fetchQuote();
-    fetchDate();
   }, []);
 
-  // https://stackoverflow.com/questions/47607666/how-to-extract-only-time-from-iso-date-format-in-javascript
-  // const formatTime = (date) => {
-  //   const isoDate = new Date(date);
-  //   console.log(date);
-  //   let result = isoDate.match(/\d\d:\d\d/);
-  //   return result[0];
-  // };
+  useEffect(() => {
+    fetchDate();
+  }, [date]);
 
   return (
     <div className="app-mobile">
-      {loading ? <p>Loading!</p> : <h5>{quote.content}</h5>}
-      <p>Good MORNING, IT'S CURRENTLY</p>
-      {console.log(date)}
-      <h1>{new Date(date.datetime).toLocaleTimeString()}</h1>
-      <p></p>
+      {loading ? (
+        <p>Loading!</p>
+      ) : (
+        <div>
+          <h5>{quote.content}</h5>
+          <h5>{quote.author}</h5>
+        </div>
+      )}
+      {new Date(date.datetime).getHours() <= 12 ? (
+        <div>
+          <HiSun />
+          <p>GOOD MORNING, IT'S CURRENTLY</p>
+        </div>
+      ) : (
+        <div>
+          <HiMoon />
+          <p>GOOD EVENING, IT'S CURENTLY</p>
+        </div>
+      )}
+      <h1>
+        {new Date(date.datetime)
+          .toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          })
+          .slice(0, -2)}
+      </h1>
     </div>
   );
 }
