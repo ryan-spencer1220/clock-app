38f825ecd1c8250802ebe4eac4b86cdb6a4c7276
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-12-11 13:24:42 -0800

    add additional styling to mobile site

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import { ReactComponent as Sun } from "./assets/desktop/icon-sun.svg";
 import { ReactComponent as Moon } from "./assets/desktop/icon-moon.svg";
 import { ReactComponent as Refresh } from "./assets/desktop/icon-refresh.svg";
-import { ReactComponent as ArrowDown } from "./assets/desktop/icon-arrow-down.svg";
 import { ReactComponent as ArrowUp } from "./assets/desktop/icon-arrow-up.svg";
 
 function App() {
   const [quote, setQuote] = useState({});
   const [date, setDate] = useState({});
+  const [moreInfo, setMoreInfo] = useState(false);
   // const [location, setLocation] = useState({});
 
   const fetchDate = async () => {
@@ -39,13 +39,14 @@ function App() {
     fetchDate();
   }, [date]);
 
+  console.log(moreInfo);
   return (
     <div className="app-mobile">
-      <div className="group5">
+      <div className="quote-container">
         <p className="quote">"{quote.content}"</p>
         <Refresh className="refresh" onClick={fetchQuote} />
-        <p className="author">{quote.author}</p>
       </div>
+      <p className="author">{quote.author}</p>
       <div className="time-container">
         <div className="welcome">
           {new Date(date.datetime).getHours() <= 12 ? (
@@ -60,24 +61,35 @@ function App() {
             </>
           )}
         </div>
-        <h1>
+        <p className="time">
           {new Date(date.datetime)
             .toLocaleTimeString([], {
               hour: "2-digit",
               minute: "2-digit",
             })
             .slice(0, -2)}
-        </h1>
+        </p>
         <p className="timezone">{date.abbreviation}</p>
         {/* <p className="location">
             {location.data.location.city.name},{" "}
             {location.data.location.country.alpha2}
           </p> */}
-        <p className="location">Portland, US</p>
+        <p className="location">IN PORTLAND, US</p>
       </div>
-      <div className="group10">
-        <div className="rectangle">
-          <p className="more">MORE</p>
+      <div className="rectangle">
+        <p className="more">MORE</p>
+        <div
+          className="arrow"
+          onClick={() => setMoreInfo(!moreInfo)}
+          style={
+            moreInfo
+              ? {
+                  transform: `rotate(180deg) scale(0.75)`,
+                  paddingBottom: "0%",
+                }
+              : { backgroundColor: "rotate(0deg)" }
+          }
+        >
           <ArrowUp />
         </div>
       </div>
